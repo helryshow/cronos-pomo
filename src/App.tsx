fadc5f6aec08e055cc66602941e8edade2a463d3
helry Araujo <helryshow@gmail.com>
2025-06-13 15:33:09 -0300

    adicionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
  import './styles/theme.css';
  import './styles/global.css';
+import { Heading } from './Componentes/Heading';
 
  export function App() {
     console.log('Hay');
 
-
-
     return (
     <div>
-    <h1>Olá Mundo!</h1>
+    <Heading/>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus sequi vel perferendis quo. 
     Facilis voluptatibus dolorem maiores repellat, voluptate ratione ut. Unde, sapiente labore a doloremque 
     corporis aut blanditiis ducimus?
