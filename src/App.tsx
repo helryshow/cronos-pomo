4938c0a70b602453edb5adcff05911080dad202d
helry Araujo <helryshow@gmail.com>
2025-06-18 12:03:56 -0300

    add lucide.dev e partes css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
  import './styles/theme.css';
  import './styles/global.css';
 import { Heading } from './Componentes/Heading';
+import { TimerIcon } from 'lucide-react';
 
  export function App() {
 
 
     return (
     <div>
-    <Heading>Olá Mundo Cruel</Heading>
+    <Heading>
+      Olá Mundo Cruel <button><TimerIcon></TimerIcon></button>  
+    </Heading>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus sequi vel perferendis quo. 
     Facilis voluptatibus dolorem maiores repellat, voluptate ratione ut. Unde, sapiente labore a doloremque 
     corporis aut blanditiis ducimus?
