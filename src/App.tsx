183e8d136db42a3d1ad4b02518c841f3f47480dc
helry Araujo <helryshow@gmail.com>
2025-06-23 11:11:44 -0300

    add de novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
  import './styles/theme.css';
  import './styles/global.css';
-import { Heading } from './Componentes/Heading';
-import { TimerIcon } from 'lucide-react';
+ import { Container} from './Componentes/Container';
+ import { Heading } from './Componentes/Heading';
 
  export function App() {
 
 
     return (
-    <div>
-    <Heading>
-      Olá Mundo Cruel <button><TimerIcon></TimerIcon></button>  
-    </Heading>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus sequi vel perferendis quo. 
-    Facilis voluptatibus dolorem maiores repellat, voluptate ratione ut. Unde, sapiente labore a doloremque 
-    corporis aut blanditiis ducimus?
-    </p>
-   </div>
+    <>
+    <Container>
+       <Heading>LOGO</Heading>
+    </Container>
+       
+       <Container>
+       <Heading>MENU</Heading>
+    </Container>
+
+    </>
    );
-}
+} 
 
