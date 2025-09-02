1346fbadec21feabfd9a49880c4e1da2ca5d7bba
helry Araujo <helryshow@gmail.com>
2025-09-02 14:53:57 -0300

    Novas Tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import { Home } from './Page/Home';
 
  import './styles/theme.css';
  import './styles/global.css';
+import type { TaskStateModel } from './Componentes/Models/TaskStateModels';
+import { useState } from 'react';
 
- 
+ const initialState: TaskStateModel = {
+    task: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15,
+    },
+ };
+
  export function App() {
-
+   const [state, setState] = useState(initialState);
 
     return (
-        <Home />
+        <Home state={state} setState={setState} />
     );
 }
 
--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -1,15 +1,21 @@
 import { Container } from "../../Componentes/Container";
 import { CountDown } from "../../Componentes/CountDown";
 import { MainForm } from "../../Componentes/MainForm";
+import type { TaskStateModel } from "../../Componentes/Models/TaskStateModels";
 import { MainTemplate } from "../../Templates/MainTemplate";
 
- export function Home() {
+type HomeProps = {
+   state: TaskStateModel,
+   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
 
-
+ export function Home(props:HomeProps) {
+const{ state, setState }= props;
+   
     return (
      <MainTemplate>
         <Container>
-               <CountDown/>
+               <CountDown formattedSecondsRemaining={state.formattedSecondsRemaining}/>
             </Container>
         
              <Container>
